6b2fd136ae67876a527be20baff8205268d525e2
nilesh94 <nilesh.sandil@live.in>
2025-12-18 02:08:51 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'navy': '#1B263B',
+        'steel': '#415A77',
+        'orange': '#F26419',
+        'platinum': '#E0E1DD',
+      },
+      animation: {
+        'marquee': 'marquee 30s linear infinite',
+        'scroll': 'scroll 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        scroll: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
