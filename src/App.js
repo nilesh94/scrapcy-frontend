41a8b783b054c8b71d4caf8e3ae84148f5c38152
nilesh94 <nilesh.sandil@live.in>
2025-12-18 15:07:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import CompanyDashboard from './pages/CompanyDashboard';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/company/dashboard" element={<CompanyDashboard />} />
         </Routes>
         
         {/* Footer stays constant across all pages */}
