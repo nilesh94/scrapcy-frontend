eb8260e0022107f10c668ef078bcf5b01f7aff42
nilesh94 <nilesh.sandil@live.in>
2026-01-29 20:08:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // <--- THIS LINE IS CRITICAL
 import App from './App';
+import './utils/axiosSetup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
